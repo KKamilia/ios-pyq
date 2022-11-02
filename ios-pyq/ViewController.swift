c74c5ed8d456db83764365211e2b4d2f0db27775
qiqi.zhao <kamiliaziu0533@163.com>
2022-11-03 00:45:34 +0800

    Initial Commit

---
+++ ios-pyq/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-pyq
+//
+//  Created by Qiqi Zhao on 2022/11/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
