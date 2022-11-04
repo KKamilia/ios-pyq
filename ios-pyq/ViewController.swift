a4c016e91b14b3e4d62c76111745b1f82abf2fe8
qiqi.zhao <kamiliaziu0533@163.com>
2022-11-04 09:56:56 +0800

    feat: adjust the layout of logout bin

--- ios-pyq/ViewController.swift
+++ ios-pyq/ViewController.swift
@@ -13,34 +13,45 @@ class TodoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //todo: add a 
     }
 }
 
 class CreateTodoViewController: UIViewController {
-    
+    @IBAction func addTodo(_sender:Any) {
+        //Todo: pass content to main list
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
 }
 
 
 class PersonalInfoViewController: UIViewController {
     
-//    @IBOutlet var delBtn: UIButton!
-
     override func viewDidLoad() {
-
+        
         super.viewDidLoad()
-        let newBtn = UIButton()
-        newBtn.setTitle("退出登陆x", for: .normal)
-        self.view.addSubview(newBtn)
-    }
-    
-    private func handleLogout(){
-
-//        delBtn.frame =
-//        self.view.addSubview(delBtn)
+        
+        let logoutBtn = UIButton()
+        
+        logoutBtn.backgroundColor = UIColor(red: 17.0/255.0, green: 119.0/255.0, blue: 151.0/255.0, alpha: 1.0)
+        logoutBtn.setTitle("退出登陆", for: .normal)
+        logoutBtn.setTitleColor(UIColor .white, for: .normal)
+        
+        //        logoutBtn.frame = CGRectMake(self.view.frame.width * 0.08,self.view.frame.height * 0.8, self.view.frame.width * 0.9, self.view.frame.height * 0.05)
+        
+        self.view.addSubview(logoutBtn)
+        
+        //contriant - position
+        logoutBtn.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            logoutBtn.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            logoutBtn.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            logoutBtn.widthAnchor.constraint(equalTo: self.view.widthAnchor,multiplier:0.8)
+        ])
+        logoutBtn.layer.cornerRadius = 1.5
+        
     }
 }
-
-
