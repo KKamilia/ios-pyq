abbb727a3a3d85f5058145371c410d8f3d2d2171
qiqi.zhao <kamiliaziu0533@163.com>
2022-11-09 11:58:45 +0800

    feat: adjust text alignment direction

--- Wechat/UI Component/Profile/View/ProfileMain/ProfileHeaderView.swift
+++ Wechat/UI Component/Profile/View/ProfileMain/ProfileHeaderView.swift
@@ -7,15 +7,34 @@
 
 import SwiftUI
 
+struct AvatarModifier: ViewModifier {
+    func body(content: Content) -> some
+    View {
+        content
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.red, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+extension Image {
+    func avater() -> some View {
+        modifier(AvatarModifier())
+    }
+}
+
 struct ProfileHeaderView: View {
     var body: some View {
         HStack {
             Image("timeline_profile_image")
                 .resizable()
+                .avater()
                 .aspectRatio(contentMode: .fit)
                 .cornerRadius(6)
                 .frame(width: 62, height: 62)
-            VStack {
+            VStack(alignment: .leading){
                 Text("桃子猪")
                     .font(.headline)
                 Text("微信号:xxxxx")
