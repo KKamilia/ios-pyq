d030840fac360e043e10d911e99ae4aa6cf19740
qiqi.zhao <kamiliaziu0533@163.com>
2022-11-17 18:10:36 +0800

    fix: comment add

--- Wechat/UI Component/Timeline/View/Content/CommentView.swift
+++ Wechat/UI Component/Timeline/View/Content/CommentView.swift
@@ -8,25 +8,18 @@
 import SwiftUI
 
 struct CommentView: View {
-    // todo: init data in ViewModel
-    var comments: [TimelineComment]
+ 
+    let name: String
+    let content: String
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 4) {
-            ForEach(comments) { comment in
-                self.load(comment: comment)
-                    .fixedSize(horizontal: false, vertical: true)
-            }
+        HStack {
+            Text(name + ":")
+                .font(.footnote)
+                .foregroundColor(.blue)
+            Text(content)
+                .font(.footnote)
         }
-        .padding(.init(top: 6, leading: 12, bottom: 6, trailing: 12))
-    }
-    
-    func load(comment: TimelineComment) -> Text {
-        Text(comment.sender)
-            .font(.system(size: 14, weight: .medium))
-            .foregroundColor(Color("link")) +
-        Text("：\(comment.content)")
-            .font(.system(size: 14))
     }
 }
 
